08b351d1fa71d668a5a810e549230d9d4cab6594
Santos Gonzalez <sgonzo3@gmail.com>
2019-10-21 23:56:40 -0400

    feat: all tests pass

--- src/App.js
+++ src/App.js
@@ -3,41 +3,104 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleKeyPress(e) {
+    e.preventDefault(e);
+    if(document.getElementById(e.key.toUpperCase()) ){
+      document.getElementById("display").innerText = e.key.toUpperCase();
+      document.getElementById(e.key.toUpperCase()).play();
+    }
+  }
+
   return (
-    <div className="App" id="drum-machine">
+    <div
+      className="App"
+      id="drum-machine"
+      tabIndex="1"
+      onKeyPress={handleKeyPress}
+      >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Drum Machine</h1>
       </header>
-      <main id="display">
-        <section class="drum-box">
-          <li id="" key="" class="drum-pad">Q</li>
-          <li id="" key="" class="drum-pad">W</li>
-          <li id="" key="" class="drum-pad">E</li>
-
-          <li id="" key="" class="drum-pad">A</li>
-          <li id="" key="" class="drum-pad">S</li>
-          <li id="" key="" class="drum-pad">D</li>
-
-          <li id="" key="" class="drum-pad">Z</li>
-          <li id="" key="" class="drum-pad">X</li>
-          <li id="" key="" class="drum-pad">C</li>
-        </section>
-
+      <main>
+        <DrumBox/>
       </main>
-      <footer></footer>
+      <footer><span>Made by <a href="">Santos Gonzalez</a></span></footer>
     </div>
   );
 }
 
+class DrumBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      padList: ["Q","W","E","A","S","D","Z","X","C"],
+      // drum files from https://codepen.io/freeCodeCamp/pen/MJyNMd
+      drumList: [
+        'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+      ],
+      otherList: [
+        'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+        'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+      ]
+    };
+  }
+  render() {
+    return(
+      <section
+        className="drum-box"
+        >
+        <article id="display">Hit a Drum Pad!</article>
+        {
+          this.state.padList.map( (drum, index) => {
+            return <DrumPad
+              key={drum}
+              id= {`drum ${index}`}
+              drum={drum}
+              index={index}
+              src={this.state.drumList[index]}
+              />
+          })
+        }
+      </section>
+    )
+  }
+}
+
+class DrumPad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleClick = (e) => {
+    document.getElementById("display").innerText = this.props.drum;
+    document.getElementById(this.props.drum).play();
+  }
+  render() {
+    return (
+      <button
+        className="drum-pad"
+        id={this.props.id}
+        onClick={this.handleClick}
+        >
+        {this.props.drum}
+        <audio id={this.props.drum} className="clip" src={this.props.src}></audio>
+      </button>
+    )
+  }
+}
 export default App;
