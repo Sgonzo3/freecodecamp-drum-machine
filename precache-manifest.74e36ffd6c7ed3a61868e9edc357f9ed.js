d7b2716a4f6ae0786861d858c14c2a47233f103e
Santos Gonzalez <sgonzo3@gmail.com>
2020-01-11 23:08:18 -0500

    Updates

---
+++ precache-manifest.74e36ffd6c7ed3a61868e9edc357f9ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c28337b42b6a4e96d03d0d5aef7cff0",
+    "url": "/freecodecamp-drum-machine/index.html"
+  },
+  {
+    "revision": "1814976cbf73788d1954",
+    "url": "/freecodecamp-drum-machine/static/css/main.2a33a673.chunk.css"
+  },
+  {
+    "revision": "becf2f1dee4af9d9a730",
+    "url": "/freecodecamp-drum-machine/static/js/2.e70a5d51.chunk.js"
+  },
+  {
+    "revision": "83b3d04082a8c50f224f3e213e6a5a45",
+    "url": "/freecodecamp-drum-machine/static/js/2.e70a5d51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1814976cbf73788d1954",
+    "url": "/freecodecamp-drum-machine/static/js/main.d6df5e48.chunk.js"
+  },
+  {
+    "revision": "b7e02205375d0bb00042",
+    "url": "/freecodecamp-drum-machine/static/js/runtime-main.0899409b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/freecodecamp-drum-machine/static/media/logo.25bf045c.svg"
+  }
+]);
